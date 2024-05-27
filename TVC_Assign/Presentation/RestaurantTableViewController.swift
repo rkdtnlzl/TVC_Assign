de5099c1895695dd82d13cd3efc139ad6b464276
rkdtnlzl <rkdtnlzl@naver.com>
2024-05-27 23:16:49 +0900

    [Feat] SearchBar 구현

--- TVC_Assign/Presentation/RestaurantTableViewController.swift
+++ TVC_Assign/Presentation/RestaurantTableViewController.swift
@@ -7,29 +7,34 @@
 
 import UIKit
 
-class RestaurantTableViewController: UITableViewController {
-
-    var list = RestaurantList()
+class RestaurantTableViewController: UITableViewController, UISearchBarDelegate {
     
-    @IBOutlet var searchTextField: UITextField!
+    var list = RestaurantList().restaurantArray
     
+    @IBOutlet var searchBar: UISearchBar!
+    
+    var filteredList : [Restaurant] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.rowHeight = 140
-
+        
+        filteredList = list
+        
+        searchBar.delegate = self
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.restaurantArray.count
+        return filteredList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantTableViewCell", for: indexPath) as? RestaurantTableViewCell else { return UITableViewCell() }
         
-        let data = list.restaurantArray[indexPath.row]
+        let data = list[indexPath.row]
         cell.nameLabel.text = data.name
         cell.nameLabel.font = .boldSystemFont(ofSize: 18)
         cell.kindLabel.text = data.category
@@ -44,13 +49,17 @@ class RestaurantTableViewController: UITableViewController {
         
         return cell
     }
-
     
-    @IBAction func searchButtonClicked(_ sender: UIButton) {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-        if searchTextField.text == "한식" {
-            
+        guard let searchText = searchBar.text, !searchText.isEmpty else {
+            filteredList = list
+            tableView.reloadData()
+            return
         }
+        
+        filteredList = list.filter { $0.name.contains(searchText) }
+        tableView.reloadData()
     }
     
 }
