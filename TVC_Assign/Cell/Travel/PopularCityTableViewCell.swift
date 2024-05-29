bccc5e08c6850840b6dbbd4f28853244e95063bb
rkdtnlzl <rkdtnlzl@naver.com>
2024-05-29 21:29:43 +0900

    [Feat] 인기도시 tableView 구현

---
+++ TVC_Assign/Cell/Travel/PopularCityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PopularCityTableViewCell.swift
+//  TVC_Assign
+//
+//  Created by 강석호 on 5/29/24.
+//
+
+import UIKit
+
+class PopularCityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TVC_Assign/Model/CityInfo.swift
@@ -0,0 +1,8 @@
+//
+//  CityInfo.swift
+//  TVC_Assign
+//
+//  Created by 강석호 on 5/29/24.
+//
+
+import Foundation
