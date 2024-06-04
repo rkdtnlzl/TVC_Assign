2428625b3a802fe891953f340ae66385eb86ab54
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-04 09:26:44 +0900

    [Feat] 채팅방 tableView UI 구현

---
+++ TVC_Assign/Cell/Talk/ChattingRoomTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ChattingRoomTableViewCell.swift
+//  TVC_Assign
+//
+//  Created by 강석호 on 6/3/24.
+//
+
+import UIKit
+
+class ChattingRoomTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var messageBox: UIView!
+    @IBOutlet var message: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setAutoLayout()
+        
+    }
+    func setAutoLayout() {
+        message.sizeToFit()
+        message.lineBreakMode = .byWordWrapping
+        message.numberOfLines = 0
+
+    }
+    
+    func configure(_ data: Chat) {
+        profileImageView.image = UIImage(named: data.user.profileImage)
+        profileImageView.contentMode = .scaleAspectFit
+        nameLabel.text = data.user.rawValue
+        nameLabel.font = .boldSystemFont(ofSize: 17)
+        message.text = data.message
+        message.font = .systemFont(ofSize: 15)
+        message.textColor = .darkGray
+    }
+
+    
+}
---
+++ TVC_Assign/Presentation/Talk/ChattingRoomViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ChattingRoomViewController.swift
+//  TVC_Assign
+//
+//  Created by 강석호 on 6/3/24.
+//
+
+import UIKit
+
+class ChattingRoomViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var tableView: UITableView!
+    
+    var filteredList : [Chat] = []
+    let chatRoom = mockChatList.first { $0.chatroomId == 1 }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        filteredList = chatRoom?.chatList ?? []
+        
+        let nib = UINib(nibName: "ChattingRoomTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "ChattingRoomTableViewCell")
+        
+        setTableView()
+    }
+
+    func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        tableView.estimatedRowHeight = 50
+        tableView.rowHeight = UITableView.automaticDimension
+
+//        textView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChattingRoomTableViewCell", for: indexPath) as! ChattingRoomTableViewCell
+        let chat =  filteredList[indexPath.row]
+        cell.configure(chat)
+        return cell
+    }
+}
